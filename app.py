dfa2af7d1baccc04ee72ffdb415537c731e3a590
Ben Green <benno.green@gmail.com>
2025-11-24 16:52:16 -0500

    Refactor to use functions and early return

--- app.py
+++ app.py
@@ -41,50 +41,58 @@ def validate_data(df: pd.DataFrame) -> bool:
     return True
 
 
-st.write("Analyze your data!")
+def main():
+    st.write("Analyze your data!")
 
-with st.container(border=True):
-    uploaded_file = st.file_uploader("Upload a data file", type=["parquet"])
+    with st.container(border=True):
+        uploaded_file = st.file_uploader("Upload a data file", type=["parquet"])
 
-if not uploaded_file:
-    st.write("Please upload a file.")
-else:
+    if not uploaded_file:
+        st.write("Please upload a file.")
+        return
+
     data = pd.read_parquet(uploaded_file)
 
-    if validate_data(data):
-        with st.container(border=True):
-            ticker = st.selectbox("Ticker", data["ticker"].unique())
-            data = data.where(data["ticker"] == ticker)
+    if not validate_data(data):
+        return
 
-        tab_names = ["Dataframe"]
-        charts = []
-        if all(column in data.columns for column in CANDLE_COLUMNS):
-            candles_chart = go.Candlestick(
-                x=data["date"],
-                open=data["open"],
-                close=data["close"],
-                high=data["high"],
-                low=data["low"],
-            )
-            candles_layout = go.Layout(
-                title=ticker, xaxis={"title": "Date"}, yaxis={"title": "Price"}
-            )
-            candles_figure = go.Figure(data=[candles_chart], layout=candles_layout)
+    with st.container(border=True):
+        ticker = st.selectbox("Ticker", data["ticker"].unique())
+        data = data.where(data["ticker"] == ticker)
 
-            tab_names.append("Candles")
-            charts.append(candles_figure)
+    tab_names = ["Dataframe"]
+    charts = []
+    if all(column in data.columns for column in CANDLE_COLUMNS):
+        candles_chart = go.Candlestick(
+            x=data["date"],
+            open=data["open"],
+            close=data["close"],
+            high=data["high"],
+            low=data["low"],
+        )
+        candles_layout = go.Layout(
+            title=ticker, xaxis={"title": "Date"}, yaxis={"title": "Price"}
+        )
+        candles_figure = go.Figure(data=[candles_chart], layout=candles_layout)
 
-        for column in data.columns:
-            if column in ("ticker", "date") or column in CANDLE_COLUMNS:
-                continue
+        tab_names.append("Candles")
+        charts.append(candles_figure)
 
-            tab_names.append(column)
-            charts.append(data[[column, "date"]].set_index("date"))
+    for column in data.columns:
+        if column in ("ticker", "date") or column in CANDLE_COLUMNS:
+            continue
 
-        tabs = st.tabs(tab_names)
-        tabs[0].dataframe(data, height=250, width="stretch")
-        for index, (tab_name, chart) in enumerate(zip(tab_names[1:], charts), start=1):
-            if isinstance(chart, go.Figure):
-                tabs[index].plotly_chart(chart)
-            else:
-                tabs[index].line_chart(chart)
+        tab_names.append(column)
+        charts.append(data[[column, "date"]].set_index("date"))
+
+    tabs = st.tabs(tab_names)
+    tabs[0].dataframe(data, height=250, width="stretch")
+    for index, chart in enumerate(charts, start=1):
+        if isinstance(chart, go.Figure):
+            tabs[index].plotly_chart(chart)
+        else:
+            tabs[index].line_chart(chart)
+
+
+if __name__ == "__main__":
+    main()
